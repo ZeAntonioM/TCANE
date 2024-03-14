aeec085f896045bc4d98949a619a8c613ba50bd5
José Martins <joseapmartins2003@gmail.com>
2024-03-14 00:34:23 +0000

    Moving button and Lab blend done

--- Assets/ButtonDown.cs
+++ Assets/ButtonDown.cs
@@ -7,37 +7,75 @@ public class ButtonDown : MonoBehaviour
 {
 
     public Transform visualTarget;
+    public Vector3 localAxis;
+    public float resetSpeed = 0.1f;
+    public float followAngleThreshold = 0.45f;
+
+    private Vector3 InitialPosition;
 
     private Vector3 offset;
     private Transform pokeAttachTransform;
 
     private XRBaseInteractor interactable;
     private bool isPressed = false;
+    private bool freeze = false;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        InitialPosition = visualTarget.position;
+
         interactable = GetComponent<XRBaseInteractor>();
         interactable.hoverEntered.AddListener(Click);
+        interactable.hoverExited.AddListener(UnClick);
+        interactable.selectEntered.AddListener(Freeze);
     }
+
 
     public void Click(BaseInteractionEventArgs onHover) {
         
         if (onHover.interactorObject is XRPokeInteractor){
             XRPokeInteractor interactor = (XRPokeInteractor)onHover.interactorObject;
-            isPressed = true;
 
             pokeAttachTransform = interactor.attachTransform;
             offset = visualTarget.position - pokeAttachTransform.position;
+        
+            float pokeAngle = Vector3.Angle(offset, visualTarget.TransformDirection(localAxis));
+
+            if (pokeAngle < followAngleThreshold) {
+                isPressed = true;
+                freeze = false;
+            }
         }
 
+    }
+
+    public void UnClick(BaseInteractionEventArgs onHover) {
+        if (onHover.interactorObject is XRPokeInteractor){
+            isPressed = false;
+        }
+    }
+
+    public void Freeze(BaseInteractionEventArgs onHover) {
+        if (onHover.interactorObject is XRPokeInteractor){
+            freeze = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(freeze) return;
+
         if (isPressed) {
-            visualTarget.position = pokeAttachTransform.position + offset;
+            Vector3 localTargetPosition = visualTarget.InverseTransformPoint(pokeAttachTransform.position + offset);
+            Vector3 constrainedLocalTargetPosition = Vector3.Project(localTargetPosition, localAxis);
+
+            visualTarget.position = visualTarget.TransformPoint(constrainedLocalTargetPosition);
+        }
+        else {
+            visualTarget.position = Vector3.Lerp(visualTarget.position, InitialPosition, Time.deltaTime * resetSpeed);
         }
     }
 }
